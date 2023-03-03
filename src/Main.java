bbf84cab544b1b07cb591431a0082de1e150c62a
Pavel Bykov <pavelbyk@outlook.com>
2023-03-03 16:49:27 +0300

    MilesMod

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной мили
-        int price = 80000;
-        int oneMile = 20;
-        // Рассчитываете количество бонусных миль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
-        int mile = price / oneMile;
-        System.out.println(mile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 80_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
